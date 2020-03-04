8d7ade44b5dd61c776267c9b312c4d7b04084b86
tanakorn0314 <tanakorn0412@gmail.com>
2020-03-04 13:20:13 +0700

    change style

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import './InputForm.css';
 
 const InputForm = props => {
     const { data, onChange } = props;
     return (
-        <div>
+        <div className='form-container'>
             <h2>Add bear</h2>
-            Name:<input type="text" onChange={(e) => onChange({ ...data, name: e.target.value })} /> <br />
-            Weight:<input type="number" onChange={(e) => onChange({ ...data, weight: e.target.value })} /> <br />
-            img:<input type="text" onChange={(e) => onChange({ ...data, img: e.target.value })} /> <br />
-            <button onClick={() => props.addBear()}>Add new bear</button>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>
+                            <input className='inpt' type="text" onChange={(e) => onChange({ ...data, name: e.target.value })} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Weight</td>
+                        <td>
+                            <input className='inpt' type="number" onChange={(e) => onChange({ ...data, weight: e.target.value })} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Image</td>
+                        <td>
+                            <input className='inpt' type="text" onChange={(e) => onChange({ ...data, img: e.target.value })} /> <br />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <button className='btn' onClick={() => props.addBear()}>CREATE</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
